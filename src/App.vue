9dc9f59c8f42d1fb3f91b501348b664c2e3b5b35
yload <991911081@qq.com>
2022-08-21 12:45:07 +0800

    todoList案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader @addTodo="addTodo"/>
+        <MyList :todos="todos"/>
+        <MyFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearAllTodo="clearAllTodo"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyHeader from "./components/MyHeader";
+import MyList from "./components/MyList";
+import MyFooter from "./components/MyFooter";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { MyHeader, MyList, MyFooter },
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem('todos')) || []
+    }
+  },
+  methods:{
+    addTodo(todoObj){
+      this.todos.unshift(todoObj)
+    },
+    checkTodo(id){
+      this.todos.forEach((todo)=>{
+        if(todo.id === id) todo.done = !todo.done
+      })
+    },
+    updateTodo(id,title){
+      this.todos.forEach((todo)=>{
+        if(todo.id === id) todo.title = title
+      })
+    },
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    checkAllTodo(done){
+      this.todos.forEach((todo)=>{
+        todo.done = done
+      })
+    },
+    clearAllTodo(){
+      this.todos = this.todos.filter((todo)=>{
+        return !todo.done
+      })
+    }
+  },
+  watch:{
+    todos:{
+      deep:true,
+      handler(value){
+      localStorage.setItem('todos',JSON.stringify(value))
+      }
+    }
+  },
+  mounted(){
+    this.$bus.$on('checkTodo',this.checkTodo)
+    this.$bus.$on('updateTodo',this.updateTodo)
+    this.$bus.$on('deleteTodo',this.deleteTodo)
+  },
+  beforeDestroy(){
+    this.$bus.$off('checkTodo','deleteTodo')
+    this.$bus.$off('updateTodo')
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    /*base*/
+    body {
+    background: #fff;
+    }
+
+    .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+        0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+    }
+
+    .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+    }
+
+    .btn-edit {
+    color: #fff;
+    background-color: #0ae2ac;
+    border: 1px solid #00ac75;
+    margin-right: 5px;
+    }
+
+    .btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+    }
+
+    .btn-edit:hover {
+    color: #fff;
+    background-color: #00b193;
+    }
+
+    .btn:focus {
+    outline: none;
+    }
+
+    .todo-container {
+    width: 600px;
+    margin: 0 auto;
+    }
+    .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    }
 </style>
---
+++ src/components/MyList.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="todo-main">
+    <transition-group
+      appear
+      name="animate__animated animate__bounce"
+      enter-active-class="animate__pulse"
+      leave-active-class="animate__fadeOut"
+      >
+        <MyItem 
+          v-for="todoObj in todos" 
+          :key="todoObj.id" 
+          :todo="todoObj" 
+        />
+    </transition-group>
+
+  </ul>
+</template>
+
+<script>
+import 'animate.css'
+import MyItem from "./MyItem.vue"
+export default {
+  name: "MyList",
+  components:{MyItem},
+  props:['todos']
+};
+</script>
+
+<style scoped>
+    /*main*/
+    .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+    }
+
+    .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+    }
+
+</style>
